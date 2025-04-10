ad494945b87649d0eebad0f630232486c310014f
mozzerin <mikhail@noumenadigital.com>
2025-04-10 15:47:53 +0200

    ST-4529 - fix typo

--- .github/scripts/validate-native-config-files.sh
+++ .github/scripts/validate-native-config-files.sh
@@ -2,15 +2,15 @@
 set -e
 
 # Run the generate-native-configs.sh script
-echo "Running generate-native-configs.sh..."
+printf "Running generate-native-configs.sh..."
 ./generate-native-configs.sh
 
 # Check for git diff
-echo "Validating that there are no changes in the repository..."
+printf "\nValidating that there are no changes in the repository..."
 if [[ -n $(git diff) ]]; then
-  echo "Error: Running generate-native-configs.sh resulted in changes to the repository."
+  printf "\nError: Running generate-native-configs.sh resulted in changes to the repository."
   exit 1
 fi
 
-echo "Validation passed: No changes detected after running generate-native-configs.sh."
+printf "Validation passed: No changes detected after running generate-native-configs.sh."
 exit 0
