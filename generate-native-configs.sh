81f32a67dc8d7a14f38cf42245cbf968335a474d
mozzerin <mikhail@noumenadigital.com>
2025-04-22 16:57:12 +0200

    ST-4520 revert generate native configs

--- generate-native-configs.sh
+++ generate-native-configs.sh
@@ -7,12 +7,6 @@ echo "Building fat jar..."
 mvn clean package -DskipTests -PbuildFatjar
 
 echo "Generating native image configurations..."
-
-commands=("help" "version" "check" "openapi" " " "verstion" "asdsadsa")
-
-for cmd in "${commands[@]}"; do
-  echo "Running command: $cmd"
-  java -agentlib:native-image-agent=config-merge-dir=npl-cli-core/src/main/resources/META-INF/native-image \
-    -cp npl-cli-core/target/npl-cli-jar-with-dependencies.jar \
-    com.noumenadigital.npl.cli.MainKt "$cmd"
-done
+java -agentlib:native-image-agent=config-merge-dir=npl-cli-core/src/main/resources/META-INF/native-image \
+  -cp npl-cli-core/target/npl-cli-jar-with-dependencies.jar \
+  com.noumenadigital.npl.cli.MainKt
