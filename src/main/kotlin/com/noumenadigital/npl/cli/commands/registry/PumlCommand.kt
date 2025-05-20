9b1a1dfb19852036e97e150516c0d4b192f14418
miketurner <mike@noumenadigital.com>
2025-05-20 09:08:57 +0200

    ST-4649 Small refactoring.

--- src/main/kotlin/com/noumenadigital/npl/cli/commands/registry/PumlCommand.kt
+++ src/main/kotlin/com/noumenadigital/npl/cli/commands/registry/PumlCommand.kt
@@ -1,10 +1,9 @@
 package com.noumenadigital.npl.cli.commands.registry
 
 import com.noumenadigital.npl.cli.ExitCode
-import com.noumenadigital.npl.cli.ExitCode.COMPILATION_ERROR
 import com.noumenadigital.npl.cli.ExitCode.GENERAL_ERROR
-import com.noumenadigital.npl.cli.ExitCode.INTERNAL_ERROR
 import com.noumenadigital.npl.cli.ExitCode.SUCCESS
+import com.noumenadigital.npl.cli.exception.CommandExecutionException
 import com.noumenadigital.npl.cli.service.ColorWriter
 import com.noumenadigital.npl.cli.service.CompilerService
 import com.noumenadigital.npl.cli.service.SourcesManager
@@ -31,14 +30,14 @@ data class PumlCommand(
     override fun execute(output: ColorWriter): ExitCode {
         try {
             if (!File(srcDir).isDirectory() || !File(srcDir).exists()) {
-                throw PumlError(GENERAL_ERROR, "Source directory does not exist or is not a directory: $srcDir")
+                throw CommandExecutionException("Source directory does not exist or is not a directory: $srcDir")
             }
 
             val outputDir = File(outputDir).resolve("puml")
             val protosMap = CompilerService(SourcesManager(srcDir)).compileAndReport(output = output).userDefinedMap
 
             if (protosMap == null) {
-                throw PumlError(COMPILATION_ERROR, "No user defined types found, check sources and try again.")
+                throw CommandExecutionException("No user defined types found, check sources and try again.")
             }
 
             val pumlFiles = generateFiles(protosMap)
@@ -50,16 +49,18 @@ data class PumlCommand(
                 try {
                     it.write(outputDir.toPath())
                 } catch (e: Exception) {
-                    throw PumlError(INTERNAL_ERROR, e.message!!)
+                    throw CommandExecutionException(e.message!!, e.cause)
                 }
             }
 
             output.success("Puml diagram generated successfully.")
-        } catch (e: PumlError) {
-            output.error(e.errorMessage)
-            return e.exitCode
+            return SUCCESS
+        } catch (e: CommandExecutionException) {
+            output.error(e.message)
+            return GENERAL_ERROR
+        } catch (e: Exception) {
+            throw CommandExecutionException("Failed to run NPL puml: ${e.message}", e)
         }
-        return SUCCESS
     }
 
     override fun createInstance(params: List<String>): CommandExecutor {
@@ -67,11 +68,6 @@ data class PumlCommand(
             params.firstOrNull() ?: parameters.find { it.name == "directory" }?.defaultValue ?: CURRENT_DIRECTORY
         return PumlCommand(srcDir = srcDir)
     }
-
-    data class PumlError(
-        val exitCode: ExitCode,
-        val errorMessage: String,
-    ) : Exception(errorMessage)
 
     companion object {
         private const val CURRENT_DIRECTORY = "."
