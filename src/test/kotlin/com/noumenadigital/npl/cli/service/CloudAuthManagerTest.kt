7c1f5d8569549bf85171e251a93dd5edfa1f4c40
Daniela <daniela.gianora@noumenadigital.com>
2025-08-19 11:46:51 +0200

    ST-4742 Added deployment using service account

--- src/test/kotlin/com/noumenadigital/npl/cli/service/CloudAuthManagerTest.kt
+++ src/test/kotlin/com/noumenadigital/npl/cli/service/CloudAuthManagerTest.kt
@@ -93,4 +93,29 @@ class CloudAuthManagerTest :
                 }
             }
         }
+
+        test("getServiceAccountAccessToken returns access token from client credentials without storing") {
+            val stubClient = object : NoumenaCloudAuthClient(NoumenaCloudAuthConfig("id","secret","http://localhost:8080")) {
+                override fun getAccessTokenByClientCredentials(serviceClientId: String, serviceClientSecret: String): TokenResponse {
+                    serviceClientId shouldBe "svc-id"
+                    serviceClientSecret shouldBe "svc-secret"
+                    return TokenResponse(refreshToken = null, accessToken = "svc-access")
+                }
+            }
+            val manager = CloudAuthManager(stubClient)
+            val token = manager.getServiceAccountAccessToken("svc-id", "svc-secret")
+            token shouldBe "svc-access"
+        }
+
+        test("getServiceAccountAccessToken throws when access token is missing") {
+            val stubClient = object : NoumenaCloudAuthClient(NoumenaCloudAuthConfig("id","secret","http://localhost:8080")) {
+                override fun getAccessTokenByClientCredentials(serviceClientId: String, serviceClientSecret: String): TokenResponse {
+                    return TokenResponse(refreshToken = null, accessToken = null)
+                }
+            }
+            val manager = CloudAuthManager(stubClient)
+            shouldThrow<CloudCommandException> {
+                manager.getServiceAccountAccessToken("svc-id", "svc-secret")
+            }
+        }
     })
